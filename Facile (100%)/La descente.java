4bf7464131c2feef3bac717ab01d26d76126ad18
Julien <Juraya@users.noreply.github.com>
2015-05-06 16:25:54 +0200

    Update La descente.java

--- Facile (100%)/La descente.java
+++ Facile (100%)/La descente.java
@@ -13,16 +13,14 @@ class Player {
             int SX = in.nextInt();
             int SY = in.nextInt();
             
-            for (int i = 0; i < Montagnes.length; i++) {
-                Montagnes[i] = in.nextInt(); // represents the height of one mountain, from 9 to 0. Mountain heights are provided from left to right.
-            }
-            
             // Recherche de la montagne la plus haute
             int max = 0;
             int place = 0;
-            for ( int i = 0; i < Montagnes.length; i++) {
-            if ( Montagnes[i] >= max && Montagnes[i]!=0) {
-                max = Montagnes[i];
+            
+            for (int i = 0; i < 8; i++) {
+                int M = in.nextInt();
+                if ( M >= max && M!=0) {
+                max = M;
                 place = i;
                 }
             }
